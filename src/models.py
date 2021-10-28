d54c144884669c2592c27d8ec2be941fcd9e8608
Jesuspestana01 <jesuspestana77@gmail.com>
2021-10-28 22:09:29 +0000

    initial commit

---
+++ src/models.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), nullable=False)
+    password = Column(String(30), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    dob = Column(String(80), nullable=False)
+    favorites = relationship('Favorite', backref='user', uselist=True)
+  
+
+    def login(self):
+        return {
+            "username": self.username,
+            "password": self.password
+        }
+
+class Favorites(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=False)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=False)
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    height = Column(Integer, nullable=False)
+    hair_color= Column(String(20), nullable=False)
+    eyes_color = Column(String(20), nullable=False)
+    homeworld = Column(String(40), nullable = False)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(120), nullable=False)
+    passangers = Column(String(80), nullable=False)
+    cargo_capacity= Column(Integer, nullable=False)
+    consumables = Column(Integer, nullable=False)
+    vehicle_class = Column(String(40), nullable = False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer, nullable=False)
+    gravity = Column(Integer, nullable=False)
+    population= Column(Integer, nullable=False)
+    terrain = Column(String(60), nullable=False)
+    planet_name = Column(String(40), nullable = False)
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
